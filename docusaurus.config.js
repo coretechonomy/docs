b03271c4f9020fe58b30565f4a3f1a93e13d881f
Erik Thomsen <ethomsen86@gmail.com>
2024-03-25 16:25:38 -0400

    Upgraded to Docusaurus V3.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,93 +1,130 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
-/** @type {import('@docusaurus/types').DocusaurusConfig} */
-module.exports = {
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: 'Core Techonomy, LLC',
   tagline: 'At Core Techonomy, our focus is how technology enables your business.',
-  url: 'https://docs.coretechonomy.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/coretechonomy.ico',
-  // stylesheets: [
-  //   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  // ],
+
+  // Set the production url of your site here
+  url: 'https://docs.coretechonomy.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'coretechonomy', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'Core Techonomy',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/coretechonomy.ico',
-      },
-      items: [
-        {
-          type: 'doc',
-          docId: 'welcome',
-          position: 'left',
-          label: 'Documentation',
-        },
-        {
-          href: 'https://github.com/coretechonomy/docs',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    algolia: {
-      // The application ID provided by Algolia
-      appId: '6F81LOD5IN',
 
-      // Public API key: it is safe to commit it
-      apiKey: 'a9cfb0574685ab9d1c45c5d9e3baa2b1',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
-      indexName: 'coretechonomy-docs',
-
-      // Optional: see doc section below
-      contextualSearch: true,
-
-      // Optional: Algolia search parameters
-      searchParameters: {},
-
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: 'search',
-
-      //... other Algolia params
-    },
-    footer: {
-      style: 'dark',
-      links: [
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} <a target=blank href="https://coretechonomy.com">Core Techonomy, LLC</a>`,
-    },
-    prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
-    },
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
+
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/', // Serve the docs at the site's root
+          sidebarPath: './sidebars.js',
           // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/coretechonomy/docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/coretechonomy/docs/edit/main/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: './src/css/custom.css',
         },
-      },
+      }),
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Core Techonomy',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/coretechonomy.ico',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Documentation',
+          },
+          // {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/coretechonomy/docs',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: '6F81LOD5IN',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'a9cfb0574685ab9d1c45c5d9e3baa2b1',
+  
+        indexName: 'coretechonomy-docs',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} <a target=blank href="https://coretechonomy.com">Core Techonomy, LLC</a>`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
 };
+
+export default config;
